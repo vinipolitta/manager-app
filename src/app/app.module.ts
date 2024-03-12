87d2cac15a7e91019d6b32522fba4844987d0ba4
vinipolitta <vinicius.politta1@gmail.com>
2024-03-11 21:45:19 -0300

    feat: update v16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -12,6 +10,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
